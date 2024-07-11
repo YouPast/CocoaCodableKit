adbb5a6889dfaa3388975f904f152fd96beb68c1
Wendell <41480456+WendellXY@users.noreply.github.com>
2024-07-11 21:47:14 +0800

    Update Options.swift

--- Define/Options.swift
+++ Define/Options.swift
@@ -1,25 +1,53 @@
 //
-//  Options.swift
-//  Define
+//  CodableKeyOptions.swift
+//  CodableKit
 //
-//  Created by WendellXY on 2024/5/13
+//  Created by WendellXY on 2024/5/14
 //  Copyright © 2024 WendellXY. All rights reserved.
 //
 
-public struct CodableKeyMacro {
-  public struct Options: OptionSet {
-    public let rawValue: Int32
+public struct CodableKeyOptions: OptionSet {
+  public let rawValue: Int32
 
-    public init(rawValue: Int32) {
-      self.rawValue = rawValue
-    }
-
-    /// The key will be ignored when encoding and decoding.
-    public static let ignored = Self(rawValue: 1 << 0)
-    /// The key will be explicitly set to `nil` (`null`) when encoding and decoding.
-    /// By default, the key will be omitted if the value is `nil`.
-    public static let explicitNil = Self(rawValue: 1 << 1)
-    /// The default options for a `CodableKey`.
-    public static let `default`: Self = []
+  public init(rawValue: Int32) {
+    self.rawValue = rawValue
   }
+
+  /// The default options for a `CodableKey`, which is equivalent to an empty set.
+  public static let `default`: Self = []
+  /// The key will be ignored when encoding and decoding.
+  public static let ignored = Self(rawValue: 1 << 0)
+  /// The key will be explicitly set to `nil` (`null`) when encoding and decoding.
+  /// By default, the key will be omitted if the value is `nil`.
+  public static let explicitNil = Self(rawValue: 1 << 1)
+  /// If the key has a custom CodableKey, a computed property will be generated to access the key; otherwise, this
+  /// option is ignored.
+  ///
+  /// For example, if you have a custom key `myKey` and the original key `key`, a computed property `myKey` will be
+  /// generated to access the original key `key`.
+  ///
+  /// ```swift
+  /// @Codable
+  /// struct MyStruct {
+  ///   @CodableKey("key", options: .generateCustomKey)
+  ///   var myKey: String
+  /// }
+  /// ```
+  ///
+  /// The generated code will be:
+  /// ```swift
+  /// struct MyStruct {
+  ///   var myKey: String
+  ///   var key: String {
+  ///     myKey
+  ///   }
+  /// }
+  /// ```
+  public static let generateCustomKey = Self(rawValue: 1 << 2)
+  /// Transcode the value between raw string and the target type. This is useful when the value needs to be converted
+  /// from a string to another type during decoding and vice versa during encoding. The type of the property must
+  /// conform to `Codable`, otherwise, a compile-time error will occur.
+  public static let transcodeRawString = Self(rawValue: 1 << 3)
+  /// Use the default value (if set) when decode or encode fails.
+  public static let useDefaultOnFailure = Self(rawValue: 1 << 4)
 }
